898dd6dd9e49bbf18a0b69ad04652b0fb8e38d35
Marv <tkad38@gmail.com>
2018-03-01 14:24:23 +0100

    mod link nav php

--- php/nav.php
+++ php/nav.php
@@ -1,11 +1,11 @@
 <nav>     
     <ul>
-        <li><a href="/Nirvana/nirvana3.php">page d'acceuil</a></li>
-        <li><a href="/Nirvana/galerie/galerie.php">galerie</a></li>
-        <li><a href="/Nirvana/destinations/destinations.php">destinations</a></li>
-        <li><a href="/Nirvana/reservation/reservation.php">réservation/prix</a></li>
-        <li><a href="/Nirvana/services/services.php">services</a></li>
-        <li><a href="/Nirvana/livre/livre.php">livre d'or</a></li>
-        <li><a href="/Nirvana/contact/contact.php">contact</a></li>
+        <li><a href="/nirvana/nirvana3.php">page d'acceuil</a></li>
+        <li><a href="/nirvana/galerie/galerie.php">galerie</a></li>
+        <li><a href="/nirvana/destinations/destinations.php">destinations</a></li>
+        <li><a href="/nirvana/reservation/reservation.php">réservation/prix</a></li>
+        <li><a href="/nirvana/services/services.php">services</a></li>
+        <li><a href="/nirvana/livre/livre.php">livre d'or</a></li>
+        <li><a href="/nirvana/contact/contact.php">contact</a></li>
     </ul>
 </nav>
